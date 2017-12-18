f61b0788c9e934820a73910874c36a97758b459d
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2017-12-18 14:18:16 -0600

    deleted non-use images and re-organized in html and javascript, removed unneccerary code.

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 function establishButtonHandlers() {
-    $('#Auto-btn').click(function() {
-        $('#Graduation-div').hide(450);
-        $('#Base-div').hide(450);
-        $('#Contact-div').hide(500);
-        $('#Programming-div').hide(450);
-        $('#ProjectsVideos-div').hide(450);
-        $('#ModifiedGames-div').hide(450);
-        $('#Websites-div').hide(450);
-        $('#Auto-div').show(500);
-    });
-
     $('#Graduation-btn').click(function() {
         $('#Auto-div').hide(450);
         $('#Base-div').hide(450);
@@ -20,40 +9,6 @@ function establishButtonHandlers() {
         $('#Websites-div').hide(450);
         $('#Graduation-div').show(500);
     });
-
-    $('#Base-btn').click(function() {
-        $('#Auto-div').hide(450);
-        $('#Graduation-div').hide(450);
-        $('#Contact-div').hide(500);
-        $('#Programming-div').hide(450);
-        $('#ProjectsVideos-div').hide(450);
-        $('#ModifiedGames-div').hide(450);
-        $('#Websites-div').hide(450);
-        $('#Base-div').show(500);
-    });
-
-    $('#Contact-btn').click(function() {
-        $('#Auto-div').hide(450);
-        $('#Graduation-div').hide(450);
-        $('#Base-div').hide(450);
-        $('#Programming-div').hide(450);
-        $('#ProjectsVideos-div').hide(450);
-        $('#ModifiedGames-div').hide(450);
-        $('#Websites-div').hide(450);
-        $('#Contact-div').show(500);
-    });
-
-    $('#Programming-btn').click(function() {
-        $('#Auto-div').hide(450);
-        $('#Graduation-div').hide(450);
-        $('#Base-div').hide(450);
-        $('#Contact-div').hide(450);
-        $('#ProjectsVideos-div').hide(450);
-        $('#ModifiedGames-div').hide(450);
-        $('#Websites-div').hide(450);
-        $('#Programming-div').show(500);
-    });
-
     $('#ProjectsVideos-btn').click(function() {
         $('#Auto-div').hide(450);
         $('#Graduation-div').hide(450);
@@ -86,10 +41,19 @@ function establishButtonHandlers() {
         $('#ModifiedGames-div').hide(450);
         $('#Websites-div').show(500);
     });
+    $('#Contact-btn').click(function() {
+        $('#Auto-div').hide(450);
+        $('#Graduation-div').hide(450);
+        $('#Base-div').hide(450);
+        $('#Programming-div').hide(450);
+        $('#ProjectsVideos-div').hide(450);
+        $('#ModifiedGames-div').hide(450);
+        $('#Websites-div').hide(450);
+        $('#Contact-div').show(500);
+    });
 }
 
 function main() {
-    // loadButtons();
     establishButtonHandlers();
 }
 
