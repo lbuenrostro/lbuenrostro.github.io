5163b062bee1c2345c057196782b7998b5596c81
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2018-01-25 16:11:13 -0600

    Made a function by puttin both div and btn classes together for all button to toggle correctly, therefore if a new btn is made theres no need to use jQuery for each one

--- index.js
+++ index.js
@@ -1,60 +1,11 @@
-function establishButtonHandlers() {
-    $('#Graduation-btn').click(function() {
-        $('#Auto-div').hide(450);
-        $('#Base-div').hide(450);
-        $('#Contact-div').hide(500);
-        $('#Programming-div').hide(450);
-        $('#ProjectsVideos-div').hide(450);
-        $('#ModifiedGames-div').hide(450);
-        $('#Websites-div').hide(450);
-        $('#Graduation-div').show(500);
-    });
-    $('#ProjectsVideos-btn').click(function() {
-        $('#Auto-div').hide(450);
-        $('#Graduation-div').hide(450);
-        $('#Base-div').hide(450);
-        $('#Contact-div').hide(450);
-        $('#Programming-div').hide(450);
-        $('#ModifiedGames-div').hide(450);
-        $('#Websites-div').hide(450);
-        $('#ProjectsVideos-div').show(500);
-    });
-
-    $('#ModifiedGames-btn').click(function() {
-        $('#Auto-div').hide(450);
-        $('#Graduation-div').hide(450);
-        $('#Base-div').hide(450);
-        $('#Contact-div').hide(450);
-        $('#Programming-div').hide(450);
-        $('#ProjectsVideos-div').hide(450);
-        $('#Websites-div').hide(450);
-        $('#ModifiedGames-div').show(500);
-    });
-
-    $('#Websites-btn').click(function() {
-        $('#Auto-div').hide(450);
-        $('#Graduation-div').hide(450);
-        $('#Base-div').hide(450);
-        $('#Contact-div').hide(450);
-        $('#Programming-div').hide(450);
-        $('#ProjectsVideos-div').hide(450);
-        $('#ModifiedGames-div').hide(450);
-        $('#Websites-div').show(500);
-    });
-    $('#Contact-btn').click(function() {
-        $('#Auto-div').hide(450);
-        $('#Graduation-div').hide(450);
-        $('#Base-div').hide(450);
-        $('#Programming-div').hide(450);
-        $('#ProjectsVideos-div').hide(450);
-        $('#ModifiedGames-div').hide(450);
-        $('#Websites-div').hide(450);
-        $('#Contact-div').show(500);
-    });
-}
-
 function main() {
-    establishButtonHandlers();
+    // SET UP CLICK LISTENERS
+    $('.headline-btns').click(function() {
+        $('.main-div-group').hide(450);
+        var id = $(this).attr('id');
+        var div = '#' + id.replace('btn', 'div');
+        $(div).show(500);
+    });
 }
 
 $(main);
